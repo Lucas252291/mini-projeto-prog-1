1368c12b25fcedea2d626352637fb1cc29441002
Lucas252291 <lucas.jlrs252291@gmail.com>
2025-05-12 22:01:58 -0300

    finalização do projeto

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-#TODO Crie os print() e variaveis necessarias 
+#Mini projeto parte 1
+print("Mini projeto parte 1")
 nome_completo = "Antonieta da Silva"
 
 dia_nascimento = 16
 mes_nascimento = 5
 ano_nascimento = 1998
 data_nascimento = f"{dia_nascimento}/{mes_nascimento}/{ano_nascimento}" 
-
-#tentando enviar 
 dia_atual = 14
 mes_atual = 5
 ano_atual = 2025
@@ -25,10 +24,8 @@ print (f"{nome_completo}, nascida em {data_nascimento}")
 print (f"{nome_completo} tem {total_de_dias_vividos} dias de vida")
 print (f"{nome_completo} tem {idade} anos de vida")
 
-#fiquei empolgada em plenas 02:15 da manhã e vou tentar fazer o 2 também kkkkkkkkkkk, 
-#vou organizar a bagunça
-#TODO criar variaveis para representar os dados financeiros
-
+#Mini projeto parte 2
+print("\nMini projeto parte 2")
 patrimonio = 2000.00
 recebimentos_mensais = 1518.00
 gastos = 1077.00
@@ -39,19 +36,23 @@ equivalente_salario_minimo = recebimentos_mensais / salario_minimo
 equivalente_gastos = gastos / recebimentos_mensais * 100
 patrimonio_em_um_mes = patrimonio + investimentos_mensais
 saldo_livre = recebimentos_mensais - gastos - investimentos_mensais
+print(f"{nome_completo} recebe mensalmente R${recebimentos_mensais:.2f}")
+print(f"Os recebimentos equivalem a {equivalente_salario_minimo} salários mínimos")
+print(f"{nome_completo} tem um patrimonio de R${patrimonio:.2f}")
+print(f"{nome_completo} gasta R${gastos:.2f} por mês")
+print(f"Os gastos equivalem a {equivalente_gastos}% da sua renda")
+print(f"{nome_completo} investe mensalmente R${investimentos_mensais:.2f}")
+print(f"{nome_completo} após 1 mês está com o patrimonio de R${patrimonio_em_um_mes:.2f}")
+print(f"O saldo de dinheiro livre no mês foi de R${saldo_livre:.2f}")
 
-#TODO colocar os prints 
-#os ele não está aceitando imprimir acentos e não estou conseguindo ajeitar o patrimonio de 2160,00
+#Mini projeto parte 3
+meses_em_um_ano = 12
+taxa_de_rendimento = 0.005
+rendimento_em_um_ano = patrimonio * ((1 + taxa_de_rendimento) ** meses_em_um_ano - 1)
+patrimonio_final = patrimonio + rendimento_em_um_ano
 
-print (f"{nome_completo} recebe mensalmente R${recebimentos_mensais:.2f}")
-print (f"Os recebimentos equivalem a {equivalente_salario_minimo} salários mínimos")
-print (f"{nome_completo} tem um patrimonio de R${patrimonio:.2f}")
-print (f"{nome_completo} gasta R${gastos:.2f} por mês")
-print (f"Os gastos equivalem a {equivalente_gastos}% da sua renda")
-print (f"{nome_completo} investe mensalmente R${investimentos_mensais:.2f}")
-print (f"{nome_completo} após 1 mês está com o patrimonio de R${patrimonio_em_um_mes:.2f}")
-print (f"O saldo de dinheiro livre no mês foi de R${saldo_livre:.2f}")
+print("\nMini projeto parte 3")
+print(f"Se {nome_completo} não investir nada,")
+print(f"após {meses_em_um_ano} meses o patrimônio")
+print(f"terá rendido R${rendimento_em_um_ano} e será R${patrimonio_final}")
 
-#miniprojeto 3 tentando
-taxa_de_rendimento = 0.01
-patrimonio_final = patrimonio * (1 + taxa_de_rendimento) ** 12
